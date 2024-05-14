0b59d4bb528ba077f60e1cfac43a02ca591f7386
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-05-14 20:53:45 +0100

    fix: corrected html entity code for dollar sign

--- components/Hero.js
+++ components/Hero.js
@@ -7,11 +7,11 @@ const Hero = () => {
     <section className='max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20'>
       <div className='flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start'>
         <h1 className='font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4'>
-          The $10 that will save you &#38;1'000's
+        The &#36;10 that will save you &#36;1,000's
         </h1>
         <p className='text-lg opacity-80 leading-relaxed'>
           Plug the leaks in your spending habits and see how much you can save.
-          And have fun along the way, saving those &#38;&#38;&#38;!
+          And have fun along the way, saving those &#36;&#36;&#36;!
         </p>
         {/* <button className='btn btn-primary btn-wide'>
           Get {config.appName}
--- components/Pricing.js
+++ components/Pricing.js
@@ -12,8 +12,7 @@ const Pricing = () => {
         <div className="flex flex-col text-center w-full mb-20">
           <p className="font-medium text-primary mb-8">Pricing</p>
           <h2 className="font-bold text-3xl lg:text-5xl tracking-tight">
-            Save hours of repetitive code and ship faster!
-          </h2>
+            One simple payment today to stop the spending traps </h2>
         </div>
 
         <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
