b96e93f4ceb9188d1d7af8fb1230361ecfe4787c
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-05-21 15:43:09 +0100

    refactor: moved all form elements into ViewFromStep config variable

--- app/wizardForm/page.js
+++ app/wizardForm/page.js
@@ -6,6 +6,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import ViewFormStep from '@/components/ViewFormStep';
 
 export const formSchema = z.object({
+	step: z.number(),
 	// item: z.string().min(1, 'Please enter an item here'),
 	regretBuying: z.string().min(1, 'Please enter an item here'),
 	withoutItem: z
@@ -29,6 +30,7 @@ const WizardForm = () => {
 	const methods = useForm({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
+			step: 1,
 			item: '',
 			regretBuying: '',
 			withOutItem: 0,
@@ -55,36 +57,7 @@ const WizardForm = () => {
 				onSubmit={handleSubmit(onSubmit)}
 				className='h-screen flex flex-col items-center justify-center'
 			>
-				<div className='flex flex-col items-center justify-center border-2 border-neutral p-8'>
-					<input
-						{...register('item')}
-						type='text'
-						placeholder='What do you want to buy?'
-						className='px-4 py-2 rounded border-2'
-					/>
-					{/* maybe make a ternary here see Fred linked in */}
-					{errors.item && <p className='text-red-500'>{errors.item.message}</p>}
-					<button
-						type='submit'
-						disabled={isSubmitting}
-						className='px-4 py-2 rounded'
-					>
-						submit
-					</button>
-				</div>
 				<ViewFormStep />
-				<div className='flex flex-col border-2 border-neutral p-8'>
-					What is the last thing you regretted buying?
-					<input
-						{...register('regretItem')}
-						type='text'
-						placeholder='The last regret...'
-						className='px-4 py-2 rounded border-2'
-					/>
-				</div>
-				<div>
-					<p>{getValues('withoutItem')}</p>
-				</div>
 			</form>
 		</FormProvider>
 	);
--- components/ViewFormStep.js
+++ components/ViewFormStep.js
@@ -1,16 +1,93 @@
 import SmileyRating from './SmileyRating';
-import config from '@/config';
+import { useFormContext } from 'react-hook-form';
 
 import React from 'react';
 
 const ViewFormStep = () => {
+	const {
+		register,
+		formState: { errors, isSubmitting }
+	} = useFormContext();
+	const stepsConfig = [
+		{
+			questionElement: (
+				<div className='flex flex-col items-center justify-center'>
+					<input
+						{...register('item')}
+						type='text'
+						placeholder='What do you want to buy?'
+						className='px-4 py-2 rounded border-2'
+					/>
+					{/* maybe make a ternary here see Fred linked in */}
+					{errors.item && <p className='text-red-500'>{errors.item.message}</p>}
+					<button
+						type='submit'
+						disabled={isSubmitting}
+						className='px-4 py-2 rounded'
+					>
+						submit
+					</button>
+				</div>
+			),
+			stateProperty: 'item',
+			smileyRatingUsed: false
+		},
+
+		{
+			questionElement: <p>How is your life without this item?</p>,
+			stateProperty: 'withoutItem',
+			smileyRatingUsed: true
+		},
+		{
+			questionElement: (
+				<p className=''>
+					In{' '}
+					<span className='bg-neutral text-neutral-content'>6 months time</span>{' '}
+					how will this item change your life?
+				</p>
+			),
+			stateProperty: 'lifeChange',
+			smileyRatingUsed: true
+		},
+		{
+			questionElement: <p className=''>How will it really change your life?</p>,
+			stateProperty: 'reallyChangeLife',
+			smileyRatingUsed: true
+		},
+		{
+			questionElement: (
+				<div className='flex flex-col'>
+					What is the last thing you regretted buying?
+					<input
+						{...register('regretItem')}
+						type='text'
+						placeholder='The last regret...'
+						className='px-4 py-2 rounded border-2'
+					/>
+				</div>
+			),
+			stateProperty: 'regretBuying',
+			smileyRatingUsed: false
+		},
+		{
+			questionElement: (
+				<p className=''>
+					If you <span className='bg-neutral text-neutral-content'>never</span>{' '}
+					owned this item, what would happen to your life?
+				</p>
+			),
+			stateProperty: 'neverOwned',
+			smileyRatingUsed: true
+		}
+	];
 	return (
 		<>
-			{config.steps.map((step) => (
-				<div key={step.stateProperty}>
+			{stepsConfig.map((step) => (
+				<div key={step.stateProperty} className='border-2 border-neutral p-8'>
 					{step.questionElement}
-
-					<SmileyRating name={step.stateProperty} />
+					{step.smileyRatingUsed === true ? (
+						<SmileyRating name={step.stateProperty} />
+					) : null}
 				</div>
 			))}
 		</>
