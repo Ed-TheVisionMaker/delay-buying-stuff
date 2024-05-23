65df03a28802e08cbbb04b3dceaddd7b99e2b16f
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-05-23 20:08:20 +0100

    style: amend selection of SmileyRating

--- components/SmileyRating.js
+++ components/SmileyRating.js
@@ -1,6 +1,6 @@
 import { useFormContext } from 'react-hook-form';
-const SmileyRating = ({ name, nextStep }) => {
-	const { setValue } = useFormContext();
+const SmileyRating = ({ name }) => {
+	const { setValue, getValues } = useFormContext();
 	const smileys = [
 		{ emoji: '😞', label: 'Very Dissatisfied', rating: 1 },
 		{ emoji: '😟', label: 'Dissatisfied', rating: 2 },
@@ -9,10 +9,7 @@ const SmileyRating = ({ name, nextStep }) => {
 		{ emoji: '😊', label: 'Very Satisfied', rating: 5 }
 	];
 
-	const handleSmileyClick = (rating) => {
-		nextStep();
-		setValue(name, rating);
-	};
+	const ratingSelected = getValues(name);
 
 	return (
 		<div className='flex justify-center mt-4'>
@@ -21,9 +18,9 @@ const SmileyRating = ({ name, nextStep }) => {
 				return (
 					<button
 						key={index}
-						onClick={() => handleSmileyClick(rating)}
+						onClick={() => setValue(name, rating)}
 						aria-label={label}
-						className={'text-4xl mx-2 hover:scale-125'}
+						className={`text-4xl ${rating === ratingSelected ? 'scale-125' : ''} mx-2 hover:scale-125`}
 					>
 						{emoji}
 					</button>
